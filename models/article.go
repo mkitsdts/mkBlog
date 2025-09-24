17c0fb67d752c998d298db16b6d0b671c22132af
mkitsdts <mkitsdts@outlook.com>
2025-09-25 07:34:53 +0800

    added:local stamp

--- models/article.go
+++ models/article.go
@@ -4,15 +4,15 @@ import "time"
 
 type ArticleSummary struct {
 	Title    string     `json:"title" gorm:"primaryKey"`
-	UpdateAt *time.Time `json:"updateAt" gorm:"autoUpdateTime"`
+	UpdateAt *time.Time `json:"updateAt" gorm:"autoUpdateTime;local"`
 	Category string     `json:"category"`
 	Summary  string     `json:"summary"`
 }
 
 type ArticleDetail struct {
 	Title    string     `json:"title" gorm:"primaryKey"`
-	CreateAt *time.Time `json:"createAt" gorm:"autoCreateTime"`
-	UpdateAt *time.Time `json:"updateAt" gorm:"autoUpdateTime"`
+	CreateAt *time.Time `json:"createAt" gorm:"autoCreateTime;local"`
+	UpdateAt *time.Time `json:"updateAt" gorm:"autoUpdateTime;local"`
 	Author   string     `json:"author"`
 	// 使用 GORM 创建 FULLTEXT 索引并指定 ngram 分词器（MySQL 5.7.6+/8.0）
 	Content string `json:"content" gorm:"type:LONGTEXT;index:ft_content,class:FULLTEXT,option:WITH PARSER ngram"`
--- models/friend.go
+++ models/friend.go
@@ -7,5 +7,5 @@ type Friend struct {
 	URL      string     `json:"url" gorm:"not null;type:varchar(255)"`
 	Avatar   string     `json:"avatar"`
 	Desc     string     `json:"desc"`
-	CreateAt *time.Time `json:"createAt"`
+	CreateAt *time.Time `json:"createAt" gorm:"autoCreateTime;local"`
 }
