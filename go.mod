0ab536840524e0d087bb98c49d98c9c90dfc6bbf
mkitsdts <mkitsdts@outlook.com>
2026-02-15 15:57:14 +0800

    chore: limit log file maxsize

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/gin-gonic/gin v1.10.1
 	github.com/quic-go/quic-go v0.56.0
 	go.yaml.in/yaml/v3 v3.0.4
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 	gorm.io/driver/mysql v1.6.0
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.30.2
--- pkg/log/log.go
+++ pkg/log/log.go
@@ -4,19 +4,35 @@ import (
 	"log/slog"
 	"mkBlog/config"
 	"os"
+
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func Init() {
-	file, err := os.OpenFile("./app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	_, err := os.OpenFile("./app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		panic("failed to open log file: " + err.Error())
 	}
+
+	logger := &lumberjack.Logger{
+		Filename:   "app.log", // 日志文件路径
+		MaxSize:    100,       // 单个文件最大 100 MB
+		MaxBackups: 5,         // 保留最多 5 个旧文件
+		MaxAge:     30,        // 保留最多 30 天
+		Compress:   true,      // 是否压缩旧日志（.gz）
+	}
+
 	var level slog.Level
 	if config.Cfg.Server.Devmode {
 		level = slog.LevelDebug
 	} else {
 		level = slog.LevelInfo
 	}
-	logger := slog.NewJSONHandler(file, &slog.HandlerOptions{Level: level})
-	slog.SetDefault(slog.New(logger))
+
+	handler := slog.NewJSONHandler(logger, &slog.HandlerOptions{
+		Level: level,
+	})
+
+	// 设置全局 logger
+	slog.SetDefault(slog.New(handler))
 }
