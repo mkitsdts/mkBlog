ff287fa4aba69ebb02e771c166b43a59c2ddfaa2
mkitsdts <mkitsdts@outlook.com>
2026-02-24 10:28:16 +0800

    chore: remove logger init refer to config file

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -2,29 +2,30 @@ package log
 
 import (
 	"log/slog"
-	"mkBlog/config"
 	"mkBlog/models"
 	"os"
+	"path"
 
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
-func Init() {
-	_, err := os.OpenFile("data/app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+func Init(flag bool) {
+	logPath := path.Join(models.Default_Data_Path, models.Default_Log_File_Path)
+	_, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		panic("failed to open log file: " + err.Error())
 	}
 
 	logger := &lumberjack.Logger{
-		Filename:   models.Default_Log_File_Path, // 日志文件路径
-		MaxSize:    100,                          // 单个文件最大 100 MB
-		MaxBackups: 5,                            // 保留最多 5 个旧文件
-		MaxAge:     30,                           // 保留最多 30 天
-		Compress:   true,                         // 是否压缩旧日志（.gz）
+		Filename:   logPath, // 日志文件路径
+		MaxSize:    100,     // 单个文件最大 100 MB
+		MaxBackups: 5,       // 保留最多 5 个旧文件
+		MaxAge:     30,      // 保留最多 30 天
+		Compress:   true,    // 是否压缩旧日志（.gz）
 	}
 
 	var level slog.Level
-	if config.Cfg.Server.Devmode {
+	if flag {
 		level = slog.LevelDebug
 	} else {
 		level = slog.LevelInfo
